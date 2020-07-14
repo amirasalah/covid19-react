738b2bc6eb76ccd2f91acc96183ac840e013fda4
amirasalah <amera.salah.89@gmail.com>
2020-07-14 13:59:28 +0200

    initial structure

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../shared/header/Header'
+import { Footer } from '../shared/footer/Footer'
+
+const App = () => {
+    return (
+        <>
+            <Header></Header>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App
